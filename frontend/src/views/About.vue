89f2fa9b3c3874a2869a7c49f11c0f84fd18055e
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2021-07-31 23:49:03 +0800

    7-31

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,78 +1,5 @@
 <template>
-  <v-footer
   
-    padless
-    absolute
-    id="cardTitle"
-  >
-    <v-card
-      flat
-      tile
-      id="cardTitle"
-    >
-      <v-card-title id="cardTitle">
-        <v-row>
-          <v-col col="3">
-          <v-img src="../pics/LOGO.png" id="logo"></v-img>
-          </v-col>
-          <v-divider vertical inset></v-divider>
-          <v-col col="6">
-          Copyright <v-icon>mdi-alpha-c-circle-outline</v-icon> 2021 Panda Singing Chinese,Inc.
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col col="3">
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4"
-            dark
-            icon
-          >
-            <v-icon size="20px">{{ icon }}</v-icon>
-          </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-title>
-      
-      <v-card-text>
-        <v-row class="mb-6" no-gutters>
-                  <v-col v-for="(item,index) in titles" :key="index" id="title">
-                  <v-card tile>{{item}}</v-card>
-                  </v-col>
-            </v-row>
-
-            <v-row no-gutters>
-                  <v-col v-for="(item,index) in row1" :key="index" id="content">
-                  <!-- <v-card class="pa-2" tile outlined> -->
-                  {{item}}
-                  <!-- </v-card> -->
-                  </v-col>
-            </v-row>
-            <v-row no-gutters>
-                  <v-col v-for="(item,index) in row2" :key="index" id="content">
-                  <!-- <v-card class="pa-2" tile outlined> -->
-                  {{item}}
-                  <!-- </v-card> -->
-                  </v-col>
-            </v-row>
-            <v-row no-gutters>
-                  <v-col v-for="(item,index) in row3" :key="index" :cols="3" id="content">
-                  <!-- <v-card class="pa-2" tile outlined> -->
-                  {{item}}
-                  <!-- </v-card> -->
-                  </v-col>
-                  <v-col><v-btn>sign up</v-btn></v-col>
-            </v-row>
-            <v-row no-gutters>
-                  <v-col v-for="(item,index) in row4" :key="index" >
-                  <!-- <v-card class="pa-2" tile outlined> -->
-                  {{item}}
-                  <!-- </v-card> -->
-                  </v-col>
-            </v-row>
-      </v-card-text> 
-    </v-card>
-  </v-footer>
 </template>
 
 <script>
@@ -95,30 +22,5 @@
 </script>
 
 <style scoped>
-#logo
-{
-  /* position:absolute;
-  width:156px; */
-  height:65px;
-}
 
-#cardTitle
-{
-  width: 100%;
-  height:30%;
-  background-color:"#ffffff";
-}
-
-#title
-{
-      font-family: Montserrat;
-      font-style: normal;
-      font-weight: bold;
-      font-size: 4px;
-      /* identical to box height, or 140% */
-      /* Light/Gray/0 */
-
-      color: #F56D7E;
-
-}
 </style>
--- frontend/src/views/Join.vue
+++ frontend/src/views/Join.vue
@@ -1,9 +1,14 @@
 <template>
 <div>
   <v-img src="../pics/join us/Frame 4608.png" id="bg"></v-img>
-  <v-card>
-    <v-card-title>Join Us</v-card-title>
-  </v-card>
+  
+    <v-card-title id="title">Join us</v-card-title>
+    <v-card-subtitle id="subtitle">Getting started from here</v-card-subtitle>
+    <v-card-content id="content" style="top:260px;">From signing up for an account to choosing an identity</v-card-content>
+    <v-card-content id="content" style="top:280px;">-Student/Teacher/Collaborator</v-card-content>
+    
+    <v-btn id="signUp" @click="toSignUp()">Sign Up Now</v-btn>
+  
   <myfooter></myfooter>
 </div>
 </template>
@@ -12,10 +17,18 @@
 import myfooter from '../components/myFooter'
 export default {
   components:{myfooter},
+  methods:(
+    {
+            toSignUp()
+            {
+                  this.$router.push('/signup');
+            },
+    }
+  )
 }
 </script>
 
-<style>
+<style scoped>
 
 #bg
 {
@@ -24,5 +37,60 @@ export default {
    position:absolute;
    left:103px;
    top:67px;
+}
+
+#signUp
+{
+  position: absolute;
+  left:865px;
+  top:334px;
+  width: 145px;
+  height: 36px;
+  
+  color:#ffffff;
+  /* Button/Primary/Active */
+
+  background: linear-gradient(0deg, rgba(13, 50, 145, 0.2) 0%, rgba(16, 54, 151, 0) 100%), #1443BD;
+  border-radius: 4px;
+}
+
+#title
+{
+  font-family: Ubuntu;
+  font-style:normal;
+  font-weight: bold;
+  color:#1A1A5E;
+  line-height: 56px;
+  font-size: 48px;
+  position:absolute;
+  left:840px;
+  top:120px;
+}
+
+#subtitle
+{
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: 0.005em;
+  font-size: 16px;
+  color:#2B2796;
+  position:absolute;
+  left:850px;
+  top:250px;
+}
+
+#content
+{
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.0025em;
+  color:#1A1A5E;
+  position:absolute;
+  left:865px;
 }
 </style>
